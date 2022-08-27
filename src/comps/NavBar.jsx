bbd2a418a7c064fbf9f6764dc8c2701d3019ced7
K4L-EL <86694528+K4L-EL@users.noreply.github.com>
2022-08-27 21:30:29 +0100

    Bootstrap Navbar

--- src/comps/NavBar.jsx
+++ src/comps/NavBar.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import Container from 'react-bootstrap/container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
-
+/**
+ * It returns a Navbar component with a container component inside of it.
+ * @returns A React element.
+ */
 let NavBar = () =>{
     return (
-        <div>
-            
-               <ul><Link to = '/about'>About</Link></ul> 
+       
+            <Navbar>
+        <Container>
+
+                <Nav.Link href = '/user'>User</Nav.Link>
+               <ul><Link to = '/about'>About</Link></ul>
                <ul> <Link to = '/user'>User</Link></ul> 
                <ul>  <Link to = '/charts'>Charts</Link></ul> 
-        </div>
+        </Container>
+        
+
+            </Navbar>
+     
     )
 }
 
